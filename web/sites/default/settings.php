40c0a6f296b71d7a3fb4b39878ff649b2e996306
Vic Tar <hotma@Bee-desktop.localdomain>
2022-12-23 16:52:30 -0500

    added tmp and private folders

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,6 +1,12 @@
 <?php
 $settings['config_sync_directory'] = '../config/default';
+$settings['file_temp_path'] = '../tmp';
+$settings['file_private_path'] = '../private';
 // #ddev-generated: Automatically generated Drupal settings file.
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include $app_root . '/' . $site_path . '/settings.ddev.php';
 }
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+   include $app_root . '/' . $site_path . '/settings.local.php';
+}
+
