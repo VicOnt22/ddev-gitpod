549cbc613fe039fba51e6ef28392d479efab41b7
VicOnt22 <github@selectfix.com>
2023-08-24 10:13:00 -0400

    added jquery_ui module

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -10,3 +10,15 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
    include $app_root . '/' . $site_path . '/settings.local.php';
 }
 
+$databases['default']['default'] = array (
+  'database' => 'dbpod',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = '5NRFKYMfxGVnBoBmDw7Jtby56ZXWe0ZnDF2HIeJVJuRsjkhkWZX-TgN4Z-KJQjWAvW8O-If2SA';
